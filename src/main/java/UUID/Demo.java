0b3c0b9accfb78d698ea30bc86cd5893a3e4dadf
Vindhya <vindhya24@outlook.com>
2024-01-18 03:52:21 +0530

    DB connectivity recap

---
+++ src/main/java/UUID/Demo.java
@@ -0,0 +1,11 @@
+package UUID;
+import java.util.*;
+
+public class Demo {
+
+    public static void main(String[] args)
+    {
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid);
+    }
+}
